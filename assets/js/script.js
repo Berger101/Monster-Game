cde18f27f900f236ae9513aba3ba6050e2b77fab
Erik Berger <berger.erik12@gmail.com>
2024-01-19 16:18:22 +0100

    JavaScript attack function

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,12 @@
  * and possible choices
  */
 let buttons = document.getElementsByTagName("button");
+let playerHealth = 100;
+let monsterHealth = 100;
+
+function getRandomValue(min, max) {
+  return Math.floor(Math.random() * (max - min)) + min;
+}
 
 /**
  * Add event listener to all the buttons
@@ -12,5 +18,34 @@ for (let button of buttons) {
     let playerChoice = this.getAttribute("data-choice");
     console.log(playerChoice);
     //playGame(playerChoice);
+
+      // Start new game
+      if (this.getAttribute("data-type") === "startNewGame") {
+        playerHealth = 100;
+        monsterHealth = 100;
+        console.log("player health: " + playerHealth);
+        console.log("monster health: " + monsterHealth);
+      }
+
+      // Game attacks
+      if (this.getAttribute("data-choice") === "attackMonster") {
+        let attackValue = getRandomValue(5, 12);
+        monsterHealth -= attackValue;
+        attackPlayer();
+        console.log("player attack: " + attackValue);
+        console.log("player health: " + playerHealth);
+        console.log("monster health: " + monsterHealth);
+      }
   });
+}
+
+function playGame(playerChoice) {
+
+  
+}
+
+function attackPlayer() {
+  let attackValue = getRandomValue(8, 15);
+  playerHealth -= attackValue;
+  console.log("monster attack: " +attackValue);
 }
