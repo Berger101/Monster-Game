34c44f23e7884df84378232cfcc5f62518d47fbf
Erik Berger <berger.erik12@gmail.com>
2024-01-22 16:18:34 +0100

    Added specialAttack in javascript

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,12 +5,27 @@
 const buttons = document.getElementsByTagName("button");
 const healthbarValuePlayer = document.querySelector('.healthbarValuePlayer');
 const healthbarValueMonster = document.querySelector('.healthbarValueMonster');
+const disabledButton = document.querySelector('.disabled');
 let playerHealth = 100;
 let monsterHealth = 100;
+let currentRound = 0;
 
 function getRandomValue(min, max) {
   return Math.floor(Math.random() * (max - min)) + min;
 }
+
+function mayUseSpecialAttack() {
+  // currentRound reset
+  if (currentRound === 4) {
+    currentRound = 0;
+  } else if (currentRound === 3) {
+    disabledButton.disabled = false;
+  }
+  else if (currentRound === 0 || currentRound === 1 || currentRound === 2 || currentRound === 4) {
+    disabledButton.disabled = true;
+  }
+}
+mayUseSpecialAttack();
 
 /**
  * Add event listener to all the buttons
@@ -24,26 +39,51 @@ for (let button of buttons) {
       if (this.getAttribute("data-type") === "startNewGame") {
         playerHealth = 100;
         monsterHealth = 100;
+        currentRound = 0;
         healthbarValuePlayer.style.width = '100%';
         healthbarValueMonster.style.width = '100%';
         console.clear();
         console.log("player health: " + playerHealth);
         console.log("monster health: " + monsterHealth);
+        console.log("current round: " + currentRound);
       }
 
-      // Game player attacks
+      // Game data choice
+      // player attacks
       if (this.getAttribute("data-choice") === "attackMonster") {
         let attackValue = getRandomValue(5, 12);
         monsterHealth -= attackValue;
         attackPlayer();
         healthbarValuePlayer.style.width = playerHealth + "%";
         healthbarValueMonster.style.width = monsterHealth + "%";
+        currentRound++;
+        mayUseSpecialAttack();
         console.log("player attack: " + attackValue);
         console.log("player health: " + playerHealth);
         console.log("monster health: " + monsterHealth);
+        console.log("current round: " + currentRound);
       }
+
+      // Special attack
+      if (this.getAttribute("data-choice") === "specialAttack") {
+        if (currentRound === 3) {
+          let attackValue = getRandomValue(10, 25);
+          monsterHealth -= attackValue;
+          attackPlayer();
+          healthbarValuePlayer.style.width = playerHealth + "%";
+          healthbarValueMonster.style.width = monsterHealth + "%";
+          currentRound = 0;
+          mayUseSpecialAttack();
+          console.log("player attack: " + attackValue);
+          console.log("player health: " + playerHealth);
+          console.log("monster health: " + monsterHealth);
+          console.log("current round: " + currentRound);
+        }
+      } 
   });
 }
+
+
 
 /**
  * Function for attacking the player each time an action is made
