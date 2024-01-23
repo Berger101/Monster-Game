f387828a69c6e947954ac8682d8979b72f4d23fd
Erik Berger <berger.erik12@gmail.com>
2024-01-23 12:26:01 +0100

    Made statement in function mayUseSpecialAttack easier

--- assets/js/script.js
+++ assets/js/script.js
@@ -19,13 +19,13 @@ function getRandomValue(min, max) {
  */
 function mayUseSpecialAttack() {
   // currentRound reset
-  if (currentRound === 4 || currentRound === 0) {
+  if (currentRound === 4) {
     currentRound = 0;
     disabledButton.disabled = true;
   } else if (currentRound === 3) {
     disabledButton.disabled = false;
   }
-  else if (currentRound === 0 || currentRound === 1 || currentRound === 2 || currentRound === 4) {
+  else if (currentRound != 3) {
     disabledButton.disabled = true;
   }
 }
